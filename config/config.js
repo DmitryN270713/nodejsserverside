a3cc730be194b64c14d46d35b499b119f622e98d
Dmitry Nikolaev <ndimon777@gmail.com>
2019-09-05 21:39:13 +0300

    Database settings added.
    This should be tested later
    
    removing unnecessary words

---
+++ config/config.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const databaseSettings = {
+    db: process.env.DB || 'weatherdb',
+    user: process.env.DB_USER || '',
+    pass: process.env.DB_PASS || '',
+    servers: (process.env.DB_SERVERS) ? process.env.DB_SERVERS.split(' ') : ['localhost:27017'],
+
+    dbParams: () => ({
+        w: "majority",
+        wtimeout: 10000,
+        j: true,
+        readPreference: 'ReadPreference.NEAREST',
+        native_parser: false
+    }),
+
+    serverParams: () => ({
+        autoReconnect: true,
+        poolSize: 7,
+        socketoptions: {
+            keepAlive: 300,
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 30000
+        }
+    }),
+
+    replsetParams: (replicaSetName = 'weatherReplica') => ({
+        replicaSet: replicaSetName,
+        ha: true,
+        haInterval: 10000,
+        poolSize: 7,
+        socketoptions: {
+            keepAlive: 300,
+            connectTimeoutMS: 30000,
+            socketTimeoutMS: 30000
+        }
+    })
+}
+
+
+module.exports = Object.assign({}, {})
